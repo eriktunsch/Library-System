d45f330b18b495edf020976800c1d244af338ef8
Anes Rebahi <anes.rebahi@avh.berlin>
2024-02-26 12:59:28 +0100

    erste bubble erstellen

--- index.php
+++ index.php
@@ -7,7 +7,21 @@ if (!$Login->isLoggedin()) {
   die;
 }
 include('php/html/menu.php'); ?>
-
+                                <li class="swiper-slide card card-slide" data-aos="fade-up" data-aos-delay="700">
+                        <div class="card-body">
+                            <div class="progress-widget">
+                                <div id="circle-progress-01" class="text-center circle-progress-01 circle-progress circle-progress-primary" data-min-value="0" data-max-value="100" data-value="90" data-type="percent">
+                                    <svg class="card-slie-arrow icon-24" width="24" viewBox="0 0 24 24">
+                              <path fill="currentColor" d="M5,17.59L15.59,7H9V5H19V15H17V8.41L6.41,19L5,17.59Z" />
+                           </svg>
+                                </div>
+                                <div class="progress-detail">
+                                    <p class="mb-2">Anzahl Bücher</p>
+                                    <h4 class="counter">x</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
   <?php
   echo $LoadResource->insertJS("intern/home.js");
 
