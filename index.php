455ee75895cdb47bbf56989d339a7dbceb7213ce
AnesRebahi <abes.rebahi@avh.berlin>
2024-03-01 10:59:22 +0100

    vorlage für ausleih daten eingefügt und columns von login seite angepasst

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ include('php/html/menu.php');?>
 <!-- nun kommt die Startseite bei Login -->
 
 <div class="row row-cols-1">
-<div class="col-lg-3 col-md-6">
+<div class="col-lg-3 col-md-4">
          <div class="card"  style="flex: 25%;">
             <div class="card-body">
                <div class="d-flex justify-content-between align-items-center">
@@ -126,7 +126,7 @@ include('php/html/menu.php');?>
             </div>
          </div>
       </div>
-      <div class="col-lg-3 col-md-6">
+      <div class="col-lg-3 col-md-4">
          <div class="card"  style="flex: 25%;">
             <div class="card-body">
                <div class="d-flex justify-content-between align-items-center">
@@ -141,7 +141,7 @@ include('php/html/menu.php');?>
             </div>
          </div>
       </div>
-      <div class="col-lg-3 col-md-6">
+      <div class="col-lg-3 col-md-4">
          <div class="card"  style="flex: 50%;">
             <div class="card-body">
                <div class="d-flex justify-content-between align-items-center">
@@ -160,19 +160,208 @@ include('php/html/menu.php');?>
 
 <div class="row row-cols-2">
     <div class="col" style="flex: 70%;">
-        <div class="card">
-            <div class="card-body p-6">
-                <h3>Informationen zur Ausleihe</h3>
-                <div class="text-center">
-                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Datum: </b> 01.03.2023 </h5>
-                    <hr>
-                    <p style = "font-size: 16px">Sehr geehrte Schülerschaft, <br>
-                        am heutigen Tag erhielten wir neue Bücher
-
-                    </p>
-                </div>
+        
+      <div class="col-sm-12">
+         <div class="card">
+            <div class="card-header d-flex justify-content-between">
+               <div class="header-title">
+                  <h4 class="card-title">Basic Table</h4>
+               </div>
             </div>
-        </div>
+            <div class="card-body p-0">
+               <div class="table-responsive mt-4">
+                  <table id="basic-table" class="table table-striped mb-0" role="grid">
+                     <thead>
+                        <tr>
+                           <th>Companies</th>
+                           <th>Members</th>
+                           <th>Budget</th>
+                           <th>Status</th>
+                           <th>Completion</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/01.png" alt="profile">
+                                 <h6>Soft UI XD Version</h6>
+                              </div>
+                           </td>
+                           <td>
+                              <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">PP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">MM</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>$14000</td>
+                           <td><div class="text-info">Pending</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>60%</h6>
+                              </div>
+                              <div class="progress bg-soft-info shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-info" data-toggle="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/02.png" alt="profile">
+                                 <h6>Add Progress Track</h6>
+                              </div>
+                           </td>
+                           <td>
+                              <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">PP</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>$3000</td>
+                           <td><div class="text-danger">Pending</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>10%</h6>
+                              </div>
+                              <div class="progress bg-soft-danger shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-danger" data-toggle="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/03.png" alt="profile">
+                                 <h6>Fix Platform Errors</h6>
+                              </div>
+                           </td>
+                           <td>
+                              <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">PP</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>Not set</td>
+                           <td><div class="text-success">Completed</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>100%</h6>
+                              </div>
+                              <div class="progress bg-soft-success shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-success" data-toggle="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/04.png" alt="profile">
+                                 <h6>Launch Our Mobile App</h6>
+                              </div>
+                           </td>
+                           <td>
+                              <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">PP</div>
+                                    </a>
+                                     <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">AP</div>
+                                    </a>
+                                     <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">DP</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>$20500</td>
+                           <td><div class="text-success">Completed</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>100%</h6>
+                              </div>
+                              <div class="progress bg-soft-success shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-success" data-toggle="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/06.png" alt="profile">
+                                 <h6>Add the New Pricing Page</h6>
+                              </div>
+                           </td>
+                           <td>
+                               <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>$500</td>
+                           <td><div class="text-primary">On Schedule</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>25%</h6>
+                              </div>
+                              <div class="progress bg-soft-primary shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-primary" data-toggle="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td>
+                              <div class="d-flex align-items-center">
+                                 <img class="rounded img-fluid avatar-40 me-3 bg-soft-primary" src="../../assets/images/shapes/01.png" alt="profile">
+                                 <h6>Redesign New Online Shop</h6>
+                              </div>
+                           </td>
+                           <td>
+                              <div class="iq-media-group iq-media-group-1">
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">SP</div>
+                                    </a>
+                                    <a href="#" class="iq-media-1">
+                                       <div class="icon iq-icon-box-3 rounded-pill">PP</div>
+                                    </a>
+                                 </div>
+                           </td>
+                           <td>$2000</td>
+                           <td><div class="text-warning">Completed</div></td>
+                           <td>
+                              <div class="d-flex align-items-center mb-2">
+                                 <h6>40%</h6>
+                              </div>
+                              <div class="progress bg-soft-warning shadow-none w-100" style="height: 6px">
+                                 <div class="progress-bar bg-warning" data-toggle="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%; transition: width 2s ease 0s;"></div>
+                              </div>
+                           </td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>
+            </div>
+         </div>
+      </div>
+   
     </div>
     <div class="col" style="flex: 30%;">
         <div class="card">
@@ -180,11 +369,11 @@ include('php/html/menu.php');?>
                 <h3>Merkliste </h3>
                 <hr>
                 <div class="text-center">
-                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Montags - Freitags:</b> 09.30 - 14.30 </h5>
+                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Harry Potter </h5>
                     <hr>
-                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Verantwortliche:</b> Frau Schmidt </h5>
+                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Corpus Delicti </h5>
                     <hr>
-                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Kontakt:</b> Email </h5>
+                    <h5 class="counter mb-2" style="visibility: visible; text-align: left"> <b> Woyzeck </h5>
                 </div>
             </div>
         </div>
